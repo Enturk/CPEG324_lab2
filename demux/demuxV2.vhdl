d36f6cd6329446429d4df7d0f41d690850def4ef
whenwhathuh <johnnypr@udel.edu>
2017-03-25 17:04:11 -0400

    demuxV2 working

--- demux/demuxV2.vhdl
+++ demux/demuxV2.vhdl
@@ -5,31 +5,35 @@ use ieee.math_real.all;
 use std.textio.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
---use ieee.std_logic_textio.all;
 
 entity demuxV2 is
   generic(width : natural := 8);
-  --generic( selWidth : natural := integer(ceil(log2(real(width)))));
   port(dIn : in std_logic;
-       --sel : in std_logic_vector(selWidth-1 downto 0); --1 to 8
-       sel : in std_logic_vector(natural(ceil(log2(real(width))))-1 downto 0);
+       sel : in std_logic_vector(natural(ceil(log2(real(width))))-1 downto 0) := x"00";
        muxOut : out std_logic_vector(width-1 downto 0));
 end demuxV2;
 
 architecture behavioral of demuxV2 is
 
-signal selSig : std_logic_vector(natural(ceil(log2(real(width-1)))) downto 0);
+--signal selSig : std_logic_vector(natural(ceil(log2(real(width-1)))) downto 0);
 begin
 
---generate select
-  process (sel) is 
-  variable my_line : line;
+  process (sel, dIn) is 
+  --variable my_line : line;
   begin
-  g:for i in sel'range loop
-    write(my_line, string'("Hello World!"));
-    writeline(output, my_line);	    
+
+  for i in 0 to width-1 loop
+    if i = to_integer(unsigned(sel)) then
+      muxOut(i) <= dIn;
+    else
+      muxOut(i) <= '0';
+    end if;
   end loop;
-  wait;
+  --g:for i in sel'range loop
+  --  write(my_line, string'("Hello World!"));
+  --  writeline(output, my_line);	    
+  --end loop;
+  --wait;
   end process;
 
 
--- demux/demuxV2_tb.vhdl
+++ demux/demuxV2_tb.vhdl
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.math_real.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity demuxV2_tb is
 end demuxV2_tb;
@@ -26,6 +28,35 @@ begin
   process
   begin
   dIn <= '1';
+  sel <= "000";
   wait for 10 ns;
+  sel <= "001";
+  wait for 10 ns;
+  sel <= "010";
+  dIn <= '0';
+  wait for 10 ns;
+  dIn <= '1';
+  wait for 10 ns;
+  sel<= "011";
+  wait for 10 ns;
+  sel <= "100";
+  wait for 10 ns;
+  sel <= "101";
+  wait for 10 ns;
+  sel <= "110";
+  wait for 10 ns;
+  sel <= "111";
+  wait for 10 ns;
+  wait;
+
   end process;
 end dataflow;
+
+
+
+
+
+
+
+
+
